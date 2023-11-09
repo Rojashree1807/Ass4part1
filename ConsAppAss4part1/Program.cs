919a4a66b9b4dff202b9b08dce200fc7456ec8a1
Rojashree1807 <rojarojashree1807@gmail.com>
2023-11-09 14:33:36 +0530

    after created

---
+++ ConsAppAss4part1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppAss4part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Rojashree";
+            int age = 22; 
+            double height = 175.5; 
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age} years");
+            Console.WriteLine($"Height: {height} cm");
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
